ee437f49cb6e1ab668ce084e878628eb51031aa9
Benedick Montales <montales.benedick@gmail.com>
2026-02-18 12:37:44 +0800

    fix: show session hint command on separate line

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -213,7 +213,8 @@ func displaySessionHint() {
 	}
 
 	fmt.Fprintf(os.Stderr, "\n")
-	fmt.Fprintf(os.Stderr, "To continue this session, run: ply -s %s\n", sessionInfo.UUID)
+	fmt.Fprintf(os.Stderr, "To continue this session, run:\n")
+	fmt.Fprintf(os.Stderr, "  ply -s %s\n", sessionInfo.UUID)
 	fmt.Fprintf(os.Stderr, "\n")
 }
 
