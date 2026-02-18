ab3c33cd0f67c90f28808b5cd1339c22ed68ef86
Benedick Montales <montales.benedick@gmail.com>
2026-02-18 12:41:29 +0800

    fix: remove leading newline from session hint

--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -212,7 +212,6 @@ func displaySessionHint() {
 		return
 	}
 
-	fmt.Fprintf(os.Stderr, "\n")
 	fmt.Fprintf(os.Stderr, "To continue this session, run:\n")
 	fmt.Fprintf(os.Stderr, "  ply -s %s\n", sessionInfo.UUID)
 	fmt.Fprintf(os.Stderr, "\n")
